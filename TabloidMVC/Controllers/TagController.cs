b5f88811b9598f9616bf33c83c567fb284cfaf14
Maria Manansala <mqmanansala@gmail.com>
2023-01-10 15:46:01 -0500

    Ticket #35, can view tag list

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class TagController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+        private readonly IPostRepository _postRepository;
+
+        public TagController(ITagRepository tagRepository, IPostRepository postRepository)
+        {
+            _tagRepository = tagRepository;
+            _postRepository = postRepository;
+        }
+
+        // GET: TagController
+        public IActionResult Index()
+        {
+            var tags = _tagRepository.GetAllTags();
+            return View(tags);
+        }
+
+        // GET: TagController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TagController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TagController/Create
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public ActionResult Create(IFormCollection collection)
+        // {
+            // try
+            // {
+                // return RedirectToAction(nameof(Index));
+            // }
+            // catch
+            // {
+                // return View();
+            // }
+        // }
+
+        // GET: TagController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TagController/Edit/5
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public ActionResult Edit(int id, IFormCollection //collection)
+        // {
+          //  try
+            // {
+               // return RedirectToAction(nameof(Index));
+            // }
+           // catch
+           // {
+              // return View();
+           // }
+        // }
+
+        // GET: TagController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TagController/Delete/5
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public ActionResult Delete(int id, IFormCollection collection)
+        // {
+          //  try
+            //{
+            //    return RedirectToAction(nameof(Index));
+           // }
+           // catch
+           // {
+               // return View();
+           // }
+        // }
+    }
+}
