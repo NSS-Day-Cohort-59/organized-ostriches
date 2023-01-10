314ddb7979879462cd5d67950f0185b49ef3e7fd
Leah Plummer <leahplumm@gmail.com>
2023-01-10 14:56:45 -0600

    added user ability to view a list of comments for a post

--- TabloidMVC/Startup.cs
+++ TabloidMVC/Startup.cs
@@ -27,6 +27,7 @@ namespace TabloidMVC
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<IPostRepository, PostRepository>();
             services.AddTransient<IUserProfileRepository, UserProfileRepository>();
+            services.AddTransient<ICommentRepository, CommentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
